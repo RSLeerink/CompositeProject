f5ec9bf9bd9d1dfe6f2dda515d146f4295bcd077
Robert <rsleerink@gmail.com>
2019-07-12 16:01:32 +0200

    Uppdates

--- MainFile.py
+++ MainFile.py
@@ -6,8 +6,14 @@ from CLT_FunctionsFile import Strain
 from CLT_FunctionsFile import Stress
 from CLT_FunctionsFile import InputData
 from CLT_FunctionsFile import FileNameDef
+from datetime import datetime
 import numpy as np
 import pandas as pd
+from pandas import DataFrame
+
+
+#Start of time tracking for script run time
+T1 = datetime.now()
 
 #FileNaming
 ProjectName = 'Test'
@@ -21,18 +27,43 @@ V21 =  V12 * (E2/E1)    #Unitless
 G12 = 3                 #Unitless
 
 LayerThickness = 1
-FiberAngle = [-45,45,0,0,0]
-NM = np.array([0,0,0,0,400,0])
+FiberAngle = [0,0,0,0]
+NM = np.array([0,0,0,0,200,0])
 
-#Calculations
-FileName = FileNameDef(ProjectName,LayuppNumber)
-InputData(E1,E2,V12,V21,G12)
+#
+#
+#   Calculations
+#
+#
+
+Options  = {'ProjectName': [ProjectName],
+            'LayuppNumber' : [LayuppNumber],
+            'TimeStamp': [0],
+            'Angle': [1],
+            }
+
+Options =  DataFrame(Options, columns = ['ProjectName','LayuppNumber','TimeStamp','Angle'])
+
+FileName = FileNameDef(Options,FiberAngle)
+InputData(E1,E2,V12,V21,G12,FiberAngle)
 LSMdf = LaminaStiffnessMatrix(E1,E2,V12,V21,G12,FiberAngle)
 LSMdf = TransformedLaminaStiffnessMatrix(LSMdf, FiberAngle)
 z = z_LaminaPosition(LayerThickness,FiberAngle)
 ABDdf = ABDMatrix(LSMdf,FiberAngle,z)
 Straindf = Strain(ABDdf,NM,z)
-Stressdf = Stress(LSMdf,Straindf)
+Stressdf = Stress(LSMdf,Straindf,z)
+Stressdf = Stressdf.reset_index()
+
+#Output to html
+#Stressdf.to_html('filename.html')
 
 #Straindf.to_csv(FileName + ' Strain.csv',index=False)
 #Stressdf.to_csv(FileName + ' Stress.csv',index=False)
+
+print()
+print('FileName:' + FileName)
+#Shows how long the script took to run
+T2 = datetime.now()
+TRun = T2 - T1
+print('Script took ' + str(TRun) +' to run') 
+print('Script ran successfully')
